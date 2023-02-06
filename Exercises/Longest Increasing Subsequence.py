0d236d5afdeabe83c78f8d1616d6bba9fbd0fb70
art1505 <arthurtalles@yahoo.com.br>
2023-02-06 14:41:21 -0300

    Código refatorado com variáveis significativas

--- Exercises/Longest Increasing Subsequence.py
+++ Exercises/Longest Increasing Subsequence.py
@@ -1,24 +1,20 @@
 class Solution:
-    # Função para encontrar o comprimento da subsequência crescente mais longa
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # Se a lista nums estiver vazia, retorna 0
+        # Verifique se a lista de entrada está vazia, retorne 0 se estiver
         if not nums:
             return 0
-        
-        # Armazena o tamanho da lista nums em n
-        n = len(nums)
-        
-        # Inicializa uma lista dp com tamanho n, com todos os valores inicialmente como 1
-        dp = [1] * n
-        
-        # Loop que varre de 1 até n-1
-        for i in range(1, n):
-            # Loop que varre todos os elementos anteriores a nums[i]
-            for j in range(i):
-                # Se nums[j] for menor que nums[i], significa que eles podem ser usados para formar uma subsequência crescente
-                if nums[j] < nums[i]:
-                    # Atualiza dp[i] como o máximo entre o valor atual de dp[i] e dp[j] + 1
-                    dp[i] = max(dp[i], dp[j] + 1)
-        
-        # Retorna o comprimento da subsequência crescente mais longa, que é o máximo valor em dp
-        return max(dp)
+
+        # Inicialize uma lista "length_of_lis" com todos os elementos definidos como 1
+        length_of_lis = [1] * len(nums)
+
+        # Percorra a lista "nums"
+        for current_index in range(len(nums)):
+            # Verifique todos os elementos antes do elemento atual
+            for previous_index in range(current_index):
+                # Se o elemento anterior for menor que o elemento atual
+                if nums[previous_index] < nums[current_index]:
+                    # Atualize o "length_of_lis" no índice atual com o valor máximo entre o valor atual e o valor anterior mais 1
+                    length_of_lis[current_index] = max(length_of_lis[current_index], length_of_lis[previous_index]+1)
+
+        # Retorne o valor máximo na lista "length_of_lis"
+        return max(length_of_lis)
